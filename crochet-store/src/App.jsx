30dce119b9e925f44bbc67b7c2d0866d66987d91
Alicia Kee <155275698+alicia-kee@users.noreply.github.com>
2025-07-15 22:56:17 -0500

    Links work in footer now. But don't go to top of each page when link is clicked. Need to create links for home page category buttons

--- crochet-store/src/App.jsx
+++ crochet-store/src/App.jsx
@@ -1,9 +1,3 @@
-// import NavigationBar from './NavigationBar.jsx'
-// import Header from './Header.jsx' 
-// import BestSellers from './BestSellers.jsx'
-// import CategoryButtons from './CategoryButtons.jsx'
-// import Footer from './Footer.jsx'
-
 import {HashRouter as Router, Routes, Route} from 'react-router-dom'
 import Home from './Pages/Home.jsx'
 import Products from './Pages/Products.jsx'
@@ -12,17 +6,6 @@ import About from './Pages/About.jsx'
 import { Layout } from './Components/Layout.jsx'
 
 function App() {
-    // return(
-    //     <div className="home-page">
-    //         <NavigationBar/>
-    //         <Header/>
-    //         <BestSellers/>
-    //         <CategoryButtons/>
-    //         <Footer/>
-    //     </div>
-    //     //enclose all components in a fragment, an empty set of angled brackets
-    // );
-
     // now all routes are child of Layout route
     return(
         <Router>
@@ -36,8 +19,6 @@ function App() {
             </Routes>
         </Router>
     );
-
-
 }
 
 export default App
--- crochet-store/src/Components/Footer.jsx
+++ crochet-store/src/Components/Footer.jsx
@@ -1,33 +1,37 @@
 import insta from '../assets/instagram-brands.svg';
 import mail from '../assets/envelope-solid.svg'
+import { Link } from 'react-router-dom';
 
 function Footer(){
+
+    const instaClick = () => {
+    window.open("https://www.instagram.com/craftie.lemen?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==", "_blank");
+    };
+
+    const emailClick = () => {
+    window.open("mailto:craftie.lemen@gmail.com", "_blank");
+    };
+
     return(
         <footer>
             <div className="footer-container">
                 <div className="contact-container">
                     <p className="footer-title">Contact:</p>
                     <div className="instagram-container">
-                        <img className="contact-icon" src={insta} alt="Instagram Logo"></img>
-                        <a href="#" className="footer-contact-link">craftie.lemen</a>
+                        <img className="contact-icon" src={insta} alt="Instagram Logo" onClick={instaClick}></img>
+                        <a className="footer-contact-link" onClick={instaClick}>craftie.lemen</a>
                     </div>
                     <div className="email-container">
-                        <img className="contact-icon" src={mail} alt="Email Logo"></img>
-                        <a href="#" className="footer-contact-link">craftie.lemen@gmail.com</a>
+                        <img className="contact-icon" src={mail} alt="Email Logo" onClick={emailClick}></img>
+                        <a href="#" className="footer-contact-link" onClick={emailClick}>craftie.lemen@gmail.com</a>
                     </div>
                 </div>
                 <div className="links-container">
-                    <p className="footer-title">Links:</p>
-                    {/* <ul>
-                        <li><a href="#" className="footer-link">Home</a></li>
-                        <li><a href="#" className="footer-link">Products</a></li>
-                        <li><a href="#" className="footer-link">Delivery Information</a></li>
-                        <li><a href="#" className="footer-link">About</a></li>
-                    </ul> */}
-                    <a href="#" className="footer-link">Home</a>
-                    <a href="#" className="footer-link">Products</a>
-                    <a href="#" className="footer-link">Delivery Information</a>
-                    <a href="#" className="footer-link">About</a>
+                    <p className="footer-title">Links:</p>                    
+                    <Link to="/" className="footer-link">Home</Link>
+                    <Link to="/Products" className="footer-link">Products</Link>
+                    <Link to="/Delivery" className="footer-link">Delivery Information</Link>
+                    <Link to="/About" className="footer-link">About</Link>
                 </div>
             </div>
             <div className="copyright-container">
