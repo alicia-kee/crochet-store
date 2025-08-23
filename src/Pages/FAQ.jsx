d33945fe4fa44ab9fc498aa5830fe88eaf1b7799
Alicia Kee <155275698+alicia-kee@users.noreply.github.com>
2025-08-22 20:24:10 -0500

    Created new FAQ page

---
+++ src/Pages/FAQ.jsx
@@ -0,0 +1,17 @@
+import Footer from '../Components/Footer.jsx'
+
+function FAQ(){
+    <div className="delivery-page">
+        <div className="delivery-body">
+            <div className="delivery-title">Delivery Information</div>
+            <div className="delivery-info-container">
+                <div className="delivery-text">You will be contacted through email following your purchase where we will arrange for a pick up or drop off of your product at an agreed location.</div>
+                <br></br>
+                <div className="delivery-text">If you have any questions, feel free to reach out to me through email or through Instagram messages!</div>
+            </div>
+        </div>
+        <Footer/>
+    </div>
+}
+
+export default FAQ
