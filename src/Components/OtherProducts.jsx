d751bdb4ce5483f24d49c9c4073c910f7a66de20
Alicia Kee <155275698+alicia-kee@users.noreply.github.com>
2025-08-23 21:15:07 -0500

    Fixed typo in Lily of the Valley product

--- src/Components/OtherProducts.jsx
+++ src/Components/OtherProducts.jsx
@@ -16,15 +16,15 @@ function OtherProducts(){
             <div className="product-category-title">✿ Other Products ✿</div>
 
             <div className="other-product-cards">
-                <ProductCard image={twoLilyPic} productName="Lilly of the Valley (2 Flowers)" price="$7.00"/>
-                <ProductCard image={fourLilyPic} productName="Lilly of the Valley (4 Flowers)" price="$12.00"/>
+                <ProductCard image={twoLilyPic} productName="Lily of the Valley (2 Flowers)" price="$7.00"/>
+                <ProductCard image={fourLilyPic} productName="Lily of the Valley (4 Flowers)" price="$12.00"/>
                 <ProductCard image={rosesPinkPic} productName="Pot of Roses (Pink)" price="$22.00"/>
                 <ProductCard image={rosesMagentaPic} productName="Pot of Roses (Magenta)" price="$22.00"/>
                 <ProductCard image={rosesPurplePic} productName="Pot of Roses (Purple)" price="$22.00"/>
                 <ProductCard image={pumpkinHatPic} productName="Pumpkin Cat Hat" price="$12.00"/>
                 <ProductCard image={muffinHatPic} productName="Muffin Cat Hat" price="$16.00"/>
                 <ProductCard image={barbequePic} productName="Barbecue Keychain" price="$13.00"/>
-                <ProductCard image={lilyKeychainPic} productName="Lilly of the Valley Keychain" price="$5.00"/>
+                <ProductCard image={lilyKeychainPic} productName="Lily of the Valley Keychain" price="$5.00"/>
                 <ProductCard image={flowerPouchPic} productName="Tulips Pouch" price="$12.00"/>
             </div>
         </div>
